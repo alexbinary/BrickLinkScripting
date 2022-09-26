955971c98fe2937bd84024c96c94d4991c20781c
Alexandre Bintz <alexandre@bintz.io>
2022-09-26 18:10:37 +0200

    disable filter by date

--- BrickLinkScripting/main.swift
+++ BrickLinkScripting/main.swift
@@ -9,7 +9,7 @@ let credentials = Credentials()
 
 updatePriceOfAllInventories(
     withTypes: [.part],
-    filter: { $0.dateCreated > "2022-05-01T05:00:00.000Z" },
+//    filter: { $0.dateCreated > "2022-05-01T05:00:00.000Z" },
     toPriceGuide: PriceGuidePath(guideType: .stock, condition: .new, quality: .avg),
     withMultiplier: 2,
     completion: {}
